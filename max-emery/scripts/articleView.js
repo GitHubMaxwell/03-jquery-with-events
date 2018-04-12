2c9676c7a431f68acb0ed5cac5b1ef906f5d096d
EmeryP <emeryparks09@hotmail.com>
2018-04-12 13:49:28 -0700

    tabs toggling

--- max-emery/scripts/articleView.js
+++ max-emery/scripts/articleView.js
@@ -59,7 +59,7 @@ articleView.handleAuthorFilter = function () {
       //show all articles
       //hide template
       $('article').show();
-      $('.template').hideClass();
+      $('.template').toggleClass('displayNoTemplate');
 
       //hide the template article and show all articles
 
@@ -73,16 +73,44 @@ articleView.handleCategoryFilter = function () {
   // When an option with a value is selected, hide all the articles, then reveal the matches.
   // When the blank (default) option is selected, show all the articles, except for the template.
   // Be sure to reset the #author-filter while you are at it! .val('');?
+  $('#category-filter').on('change', function () { // event delegation
+
+    // REVIEW: Inside this function, "this" is the element that triggered the event handler function we are defining. "$(this)" is using jQuery to select that element (analogous to event.target that we have seen before), so we can chain jQuery methods onto it.
+
+    if ($(this).val()) {
+
+      // TODO: If the <select> menu was changed to an option that has a value, we first need to hide all the articles, and then show just the ones that match for the author that was selected.
+      // Use an "attribute selector" to find those articles, and fade them in for the reader.
+      //event.currentTarget property // how to write teh callback function that passes the eveent object
+
+      $('article').hide();
+      $(`article[data-category="${$(this).val()}"]`).show();
+
+    } else {
+      // TODO: If the <select> menu was changed to an option that is blank, we should first SHOW ALL the articles, EXCEPT the one article we are using as a template.
+      //show all articles
+      //hide template
+      $('article').show();
+      $('.template').toggleClass('displayNoTemplate');
+
+      //hide the template article and show all articles
+
+    }
+    $('#author-filter').val('');
+  });
 
 };
 
 articleView.handleMainNav = function () {
-  // TODO: Add an event handler to .main-nav elements that will power the Tabs feature.
-  // Clicking any .tab element should hide all the .tab-content sections, and then reveal the single .tab-content section that is associated with the clicked .tab element.
-  // So: You need to dynamically build a selector string with the correct ID, based on the data available to you on the .tab element that was clicked.
 
-  // REVIEW: Now trigger a click on the first .tab element, to set up the page.
-  $('.main-nav .tab:first').click();
+  $('.main-nav .tab').on('click', function () { 
+    
+    $('.tab-content').hide();
+
+    $('#' + $(this).data('content')).show(); 
+
+  });
+
 };
 
 articleView.setTeasers = function () {
