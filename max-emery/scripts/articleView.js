5ffb73a91adef521978d9dc7cb632ee3ef6a388d
EmeryP <emeryparks09@hotmail.com>
2018-04-16 21:51:41 -0700

    teasers working properly

--- max-emery/scripts/articleView.js
+++ max-emery/scripts/articleView.js
@@ -118,9 +118,16 @@ articleView.setTeasers = function () {
   $('.article-body *:nth-of-type(n+2)').hide();
 
   // TODO: Add an event handler to reveal all the hidden elements, when the .read-on link is clicked. You can go ahead and hide the "Read On" link once it has been clicked. Be sure to prevent the default link-click action!
-  $('#articles a').on('click', function(){
-    $('.article-body *:nth-of-type(n+1)').show();
-    $('#articles a').hide();
+  $('#articles').on('click', 'a.read-on', function(e){
+    e.preventDefault();
+    if($(this).text() === 'Read on →'){
+      $(this).parent().find('*').fadeIn();
+      $(this).html('Show Less &larr;');
+    }else{
+      $('body').animate({ scrollTop: $(this).parent().offset().top}, 200);
+      $(this).html('Read on &rarr;');
+      $(this).parent().find('.article-body *:nth-of-type(n+2)').hide();
+    }
   });
   // Ideally, we'd attach this as just one event handler on the #articles section, and let it process (in other words... delegate) any .read-on clicks that happen within child nodes.
 };
